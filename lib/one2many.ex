1fa3e7f4a62bb00eb69b41fcef2a05a0518b8bbe
bromokun <bromokun@gmail.com>
2017-06-06 15:20:08 +0700

    updates

--- lib/one2many.ex
+++ lib/one2many.ex
@@ -14,7 +14,7 @@ defmodule One2many do
       supervisor(One2many.Endpoint, []),
       # Start your own worker by calling: One2many.Worker.start_link(arg1, arg2, arg3)
       # worker(One2many.Worker, [arg1, arg2, arg3]),
-      worker(One2many.RoomManager, [])
+      supervisor(One2many.RoomManager, [])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
